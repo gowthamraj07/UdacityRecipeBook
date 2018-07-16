b7cc56bb8d90bffd3e96c0a23b4c613ea7f5a597
Gowtham Shanmugaraj Ganesan <gowthamraj07@gmail.com>
2018-07-16 21:00:08 +0200

    Ingredients screen updated with business logic and up button enabled

--- app/src/main/java/com/asanam/udacityrecipebook/IngredientsActivity.java
+++ app/src/main/java/com/asanam/udacityrecipebook/IngredientsActivity.java
@@ -1,11 +1,18 @@
 package com.asanam.udacityrecipebook;
 
+import android.database.Cursor;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.asanam.udacityrecipebook.db.DBContract;
+import com.asanam.udacityrecipebook.provider.RecipeProvider;
 
 public class IngredientsActivity extends AppCompatActivity {
 
@@ -15,37 +22,65 @@ public class IngredientsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_ingredients);
 
         RecyclerView rvIngredients = findViewById(R.id.rv_ingredients_list);
-        Long recipeId = 1l;
+        Long recipeId = getIntent().getLongExtra("RECIPE_ID", 0l);
         rvIngredients.setAdapter(new IngredientsAdapter(recipeId));
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            // Respond to the action bar's Up/Home button
+            case android.R.id.home:
+                finish();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
     private class IngredientsAdapter extends RecyclerView.Adapter<IngredientsViewHolder> {
-        private Long recipeId;
+
+        private final Cursor query;
 
         public IngredientsAdapter(Long recipeId) {
-            this.recipeId = recipeId;
+            query = getContentResolver().query(RecipeProvider.INGREDIENTS_URI.buildUpon().appendPath(recipeId.toString()).build(),
+                    null, null, null, null);
         }
 
         @NonNull
         @Override
         public IngredientsViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-            return null;
+            View tvIngredientItem = LayoutInflater.from(parent.getContext()).inflate(R.layout.ingredient_list_item_layout, null);
+            return new IngredientsViewHolder(tvIngredientItem);
         }
 
         @Override
         public void onBindViewHolder(@NonNull IngredientsViewHolder holder, int position) {
-
+            query.moveToPosition(position);
+            holder.bind(query);
         }
 
         @Override
         public int getItemCount() {
-            return 0;
+            return query.getCount();
         }
     }
 
     private class IngredientsViewHolder extends RecyclerView.ViewHolder {
+        private TextView itemView;
+
         public IngredientsViewHolder(View itemView) {
             super(itemView);
+            this.itemView = (TextView) itemView.findViewById(R.id.tv_ingredient_item);
+        }
+
+        public void bind(Cursor query) {
+            itemView.setText(query.getString(query.getColumnIndex(DBContract.IngredientsTable.COLUMN_INGREDIENT)));
         }
     }
 }
--- app/src/main/java/com/asanam/udacityrecipebook/fragments/RecipeDetailsFragment.java
+++ app/src/main/java/com/asanam/udacityrecipebook/fragments/RecipeDetailsFragment.java
@@ -1,5 +1,6 @@
 package com.asanam.udacityrecipebook.fragments;
 
+import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
@@ -11,6 +12,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.asanam.udacityrecipebook.IngredientsActivity;
 import com.asanam.udacityrecipebook.R;
 import com.asanam.udacityrecipebook.adapter.StepsAdapter;
 import com.asanam.udacityrecipebook.dto.Step;
@@ -24,6 +26,7 @@ public class RecipeDetailsFragment extends Fragment implements DetailsView {
     private DetailsPresenter presenter;
     private RecyclerView rvSteps;
     private StepSelectionListener listener;
+    private long recipeId;
 
     @Nullable
     @Override
@@ -31,6 +34,8 @@ public class RecipeDetailsFragment extends Fragment implements DetailsView {
         View detailsView = inflater.inflate(R.layout.recipe_details_fragment_layout, container, false);
 
         rvSteps = detailsView.findViewById(R.id.rv_steps);
+        View ingredients = detailsView.findViewById(R.id.inc_ingredients);
+        ingredients.setOnClickListener(new IngredientsClickListener());
 
         if(getActivity() instanceof StepSelectionListener) {
             listener = (StepSelectionListener) getActivity();
@@ -47,8 +52,8 @@ public class RecipeDetailsFragment extends Fragment implements DetailsView {
     @Override
     public void onResume() {
         super.onResume();
-        long recipeId = getArguments().getLong("RECIPE_ID");
-        Log.i(RecipeDetailsFragment.class.getSimpleName(), "recipeId : "+recipeId);
+        recipeId = getArguments().getLong("RECIPE_ID");
+        Log.i(RecipeDetailsFragment.class.getSimpleName(), "recipeId : "+ recipeId);
         presenter.getSteps(recipeId);
     }
 
@@ -60,5 +65,14 @@ public class RecipeDetailsFragment extends Fragment implements DetailsView {
 
     public interface StepSelectionListener {
         void onSelectStep(long recipeId, long id);
+    }
+
+    private class IngredientsClickListener implements View.OnClickListener {
+        @Override
+        public void onClick(View view) {
+            Intent intent = new Intent(getActivity(), IngredientsActivity.class);
+            intent.putExtra("RECIPE_ID", recipeId);
+            getContext().startActivity(intent);
+        }
     }
 }
