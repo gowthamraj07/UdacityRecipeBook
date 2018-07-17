b465e0c644d3035cb9248b44f51fa6df06aec45f
Gowtham Shanmugaraj Ganesan <gowthamraj07@gmail.com>
2018-07-17 23:47:45 +0200

    Lint warning handled

--- app/src/main/java/com/asanam/udacityrecipebook/MainActivity.java
+++ app/src/main/java/com/asanam/udacityrecipebook/MainActivity.java
@@ -2,9 +2,9 @@ package com.asanam.udacityrecipebook;
 
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
+import android.os.Bundle;
 import android.support.test.espresso.idling.CountingIdlingResource;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
@@ -13,8 +13,8 @@ import com.asanam.udacityrecipebook.network.HttpNetworkApi;
 import com.asanam.udacityrecipebook.network.NetworkApi;
 import com.asanam.udacityrecipebook.presenter.RecipeCardsPresenter;
 import com.asanam.udacityrecipebook.repository.NetworkRecipeRepository;
-import com.asanam.udacityrecipebook.repository.RecipeDBRepository;
 import com.asanam.udacityrecipebook.repository.RecipeRepository;
+import com.asanam.udacityrecipebook.utils.Constants;
 import com.asanam.udacityrecipebook.view.AndroidRecipeCardsView;
 import com.asanam.udacityrecipebook.view.RecipeCardsView;
 
@@ -22,8 +22,7 @@ public class MainActivity extends AppCompatActivity {
 
     CountingIdlingResource countingIdlingResource = new CountingIdlingResource("DOWNLOAD");
 
-    public static final String TAG = "MainActivity";
-    private RecipeCardsPresenter presenter;
+    public static final String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,20 +38,20 @@ public class MainActivity extends AppCompatActivity {
 
         NetworkApi api = new HttpNetworkApi();
         RecipeRepository repository = new NetworkRecipeRepository(api, getContentResolver(), new NetworkListener());
-        presenter = new RecipeCardsPresenter(view, repository);
+        RecipeCardsPresenter presenter = new RecipeCardsPresenter(view, repository);
 
         countingIdlingResource.increment();
         presenter.showCards();
     }
 
     private void showRecipeActivity(long recipeId) {
-        Intent intent = null;
+        Intent intent;
         if (getResources().getBoolean(R.bool.is_tablet)) {
             intent = new Intent(this, TabletDetailsActivity.class);
         } else {
             intent = new Intent(this, DetailsActivity.class);
         }
-        intent.putExtra("RECIPE_ID", recipeId);
+        intent.putExtra(Constants.RECIPE_ID, recipeId);
         intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
 
         startActivity(intent);
@@ -71,7 +70,7 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public void onSuccess() {
             if(getIntent() != null) {
-                long recipeId = getIntent().getLongExtra("RECIPE_ID", -1);
+                long recipeId = getIntent().getLongExtra(Constants.RECIPE_ID, -1);
                 Log.d(TAG, "onCreate: recipeId : "+recipeId);
                 if(recipeId != -1) {
                     showRecipeActivity(recipeId);
